a42e4d2c6c531ea9ed65a06fb37f80a952725596
Exousia KASEKA <kasekaexousia@gmail.com>
2024-06-11 19:02:14 +0100

    validation des champs de saisie pour ajouter un contact

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-function addArticleValidations(){
+function addArticleValidations() {
   return [
-    body("title").escape().isLength({ min: 5, max: 255 }).withMessage("Le nom doit avoir entre 5 et 255 caracteres"),
-  ]
+    body("title")
+      .escape()
+      .isLength({ min: 5, max: 255 })
+      .withMessage("Le nom doit avoir entre 5 et 255 caractères"),
+    body("author")
+      .escape()
+      .isLength({ min: 2, max: 50 })
+      .withMessage("Le nom doit avoir entre 5 et 255 caractères"),
+    body("urlToImage").isURL().withMessage("not an url"),
+    body("description")
+      .escape()
+      .isLength({ min: 5, max: 500 })
+      .withMessage("Le nom doit avoir entre 5 et 255 caractères"),
+    body("content")
+      .escape()
+      .isLength({ min: 5, max: 500 })
+      .withMessage("Le nom doit avoir entre 5 et 255 caractères"),
+  ];
 }
-
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -50,6 +65,24 @@ app.post("/articles", addArticleValidations(), (req, res) => {
 
   res.send("ok");
 });
+
+
+
+// // Route pour supprimer un article
+// app.delete("/articles/:slug", (req, res) => {
+//   const { slug } = req.params;
+
+//   // Trouvez l'index de l'article dans le tableau
+//   const articleIndex = articles.findIndex((article) => article.slug === slug);
+
+//   if (articleIndex !== -1) {
+//     // Supprimez l'article du tableau
+//     articles.splice(articleIndex, 1);
+//     res.redirect("/articles"); // Redirigez vers la liste des articles
+//   } else {
+//     res.render("404"); // Affichez une page 404 si l'article n'est pas trouvé
+//   }
+// });
 
 app.get("/articles/:slug", (req, res) => {
   const { slug } = req.params;
---
+++ public/js/updateArticle.js
@@ -0,0 +1,7 @@
+const deleteButton = document.querySelector('.delete-article button');
+
+deleteButton.addEventListener('click', deleteArticle);
+
+const deleteArticle = (articleSlug) => {
+    
+}
