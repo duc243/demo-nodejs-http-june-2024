4807833239ee1b20968d6b80e33165680d0acea1
Exousia KASEKA <kasekaexousia@gmail.com>
2024-06-12 07:51:07 +0100

    suppression de l'article

--- public/js/deleteArticle.js
+++ public/js/deleteArticle.js
@@ -3,31 +3,31 @@ const deleteButton = document.getElementById("delete-article");
 deleteButton.addEventListener("click", async () => {
   const confirmation = confirm("Voulez-vous vraiment supprimer cet article ?");
 
-  if (confirmation) {
-    const title = deleteButton
-      .closest("body")
-      .querySelector("h1")
-      .innerText.toString();
-    const slug = title
-      .toLowerCase()
-      .replace(/[^\w\s-]/g, "") 
-      .replace(/\s+/g, "-") 
-      .trim();
+  if (confirmation) deleteArticle();
 
-    console.log(slug);
-
-    try {
-      const response = await fetch(`/article/${slug}`, {
-        method: 'DELETE',
-      });
-
-      if (response.ok) {
-        window.location.href = '/articles';
-      } else {
-        console.error('Erreur lors de la suppression de l\'article');
-      }
-    } catch (error) {
-      console.error('Erreur réseau :', error);
-    }
-  }
 });
+
+
+const deleteArticle = async () => {
+
+  const title = deleteButton
+    .closest("body")
+    .querySelector("h1")
+    .innerText.toString();
+  const slug = title
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "") 
+    .replace(/\s+/g, "-") 
+    .trim();
+
+  console.log(slug);
+
+  try {
+    const response = await fetch(`/articles/${slug}`, {method: "DELETE"});
+
+    window.location.href = "/articles"
+
+  } catch (error) {
+    console.error('Erreur réseau :', error);
+  }
+}
